a22b634ac061d74d91522352ad6f8b0c3e5aafc6
Cristian Ionita <cristian.ionita85@gmail.com>
2019-08-18 22:36:33 +0300

    added boot section address for text segment

--- src/build.sh
+++ src/build.sh
@@ -2,8 +2,8 @@
 
 ./clean.sh
 
-avr-gcc main.c memory.c io.c -mmcu=atmega168 -Os -g -o ../bin/avr-bootldr.elf -Wall -pedantic -nostartfiles -pipe
-avr-objcopy -j .text -j .data -O binary ../bin/avr-bootldr.elf ../bin/avr-bootldr.bin
+avr-gcc main.c memory.c io.c -mmcu=atmega1284 -Os -g -o ../bin/avr-bootldr.elf -Wall -Wl,--relax -Wl,--section-start=.text=0x1f000 -Wl,--section-start=.version=0x1fffe -pipe
+avr-objcopy -j .text -j .data -O ihex ../bin/avr-bootldr.elf ../bin/avr-bootldr.hex
 avr-size ../bin/avr-bootldr.elf
-stat ../bin/avr-bootldr.bin
+
  
